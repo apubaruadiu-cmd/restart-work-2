11e31435daf979e78304f93f79ee181ee2d82c6e
Apu Barua <apu@primetechbd.com>
2026-03-05 10:19:44 +0600

    Task complete toastify

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { toast } from "react-toastify";
 
-const Task = ({ ticket, resolve, setResolve, progress, setProgress, selectedTicket, setSelectedTickets, resolvedTasks, setResolvedTasks }) => {
-  const handleResolve = (tkt) => {
+const Task = ({ ticket, setResolve, setProgress, setSelectedTickets, setResolvedTasks }) => {
+  const handleResolve = () => {
     // Remove from Task Status
     setSelectedTickets((prev) => prev.filter((t) => t.id !== ticket.id));
     // decrease progress
@@ -11,12 +12,13 @@ const Task = ({ ticket, resolve, setResolve, progress, setProgress, selectedTick
     setResolvedTasks((prev) => [...prev, ticket]);
     // increase resolved count
     setResolve((prev) => prev + 1); 
+    toast("Task Completed!")
   }
   return (
     <div>
       <div className="p-4 rounded bg-white shadow-[0_4px_16px_0_rgba(0,0,0,0.08)]">
         <div className="text-lg font-medium text-[#001931]">{ticket.title}</div>
-        <button onClick={() => handleResolve(ticket)} className="px-4 py-3 rounded-sm text-white bg-[#02A53B] w-full mt-4">
+        <button onClick={() => handleResolve()} className="px-4 py-3 rounded-sm text-white bg-[#02A53B] w-full mt-4">
           Complete
         </button>
       </div>
